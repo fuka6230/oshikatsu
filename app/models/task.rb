719af0a0a041d174acc1bc90b583f1e08f54a2ff
fuka <hakutomato17@gmail.com>
2022-05-31 17:29:47 +0900

    first commit

---
+++ app/models/task.rb
@@ -0,0 +1,13 @@
+class Task < ApplicationRecord
+  validates :name, presence: true
+  validates :name, length: { maximum: 30 }
+  validate :validate_name_not_including_commma
+
+  belongs_to :user
+
+  private
+
+  def validate_name_not_including_commma
+    errors.add(:name, 'にカンマをふくめることはできません') if name&.include?(',')
+  end
+end
---
+++ db/migrate/20220524055954_create_tasks.rb
@@ -0,0 +1,12 @@
+class CreateTasks < ActiveRecord::Migration[7.0]
+  def change
+    create_table :tasks do |t|
+      t.string :name, null: false
+      t.string :email, null: false
+      t.string :password_digest, null: false
+
+      t.timestamps
+      t.index :email, unique: true
+    end
+  end
+end
